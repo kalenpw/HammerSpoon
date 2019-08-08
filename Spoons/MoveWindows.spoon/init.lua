29e9473d6713248c91561fd950da218d63a160a4
kwilliams <kwilliams@apgwest.com>
2019-08-08 16:07:38 -0600

    Retile after moving window

--- Spoons/MoveWindows.spoon/init.lua
+++ Spoons/MoveWindows.spoon/init.lua
@@ -8,12 +8,18 @@ function obj:init()
     hs.hotkey.bind({"shift", "alt"}, "right", function()
         focused = hs.window.focusedWindow()
         focused:moveOneScreenEast()
+        reTile()
     end)
-    
+
     hs.hotkey.bind({"shift", "alt"}, "left", function()
         focused = hs.window.focusedWindow()
         focused:moveOneScreenWest()
+        reTile()
     end)
+end
+
+function reTile()
+    hs.eventtap.keyStroke({"alt", "shift"}, "Z")
 end
 
 return obj
